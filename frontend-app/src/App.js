9bbdae5df93723437f2910369c8ab960cd71ac2b
progIfe <ifeoluwasamson90@gmail.com>
2023-04-24 10:45:08 +0100

    React Router Implementation

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,5 +1,5 @@
 import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -11,7 +11,10 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/home' element={<HomeScreen />} exact />
+          </Routes>
+          {/* <HomeScreen /> */}
           {/* <Route path='/' component={HomeScreen} exact /> */}
         </Container>
       </main>
--- frontend-app/src/components/Product.js
+++ frontend-app/src/components/Product.js
@@ -10,7 +10,7 @@ function Product({ product }) {
         </a>
 
         <Card.Body>
-            <a href={'/product/${product._id}'}>
+            <a href={`/product/${product._id}`}>
                 <Card.Title as='div'>
                     <strong>{product.name}</strong>
                 </Card.Title>
