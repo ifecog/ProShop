dab3c4192c0f251cf5d679bf759b3cea580f0f0b
progIfe <ifeoluwasamson90@gmail.com>
2023-04-25 14:49:29 +0100

    REST API Setup and products endpoint configuration (without database)

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.get_routes, name='routes'),
     path('products/', views.get_products, name='products'),
+    path('products/<str:pk>/', views.get_product_detail, name='product-detail'),
 ]
