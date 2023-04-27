ccbbf05ecbcaaa4059029af99207e354764ff5c2
progIfe <ifeoluwasamson90@gmail.com>
2023-04-27 06:13:54 +0100

    Data fetching from the backend useing React Hooks, axios, and Django Cors-hearders

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,5 +126,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- frontend-app/src/screens/ProductScreen.js
+++ frontend-app/src/screens/ProductScreen.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 function ProductScreen() {
+  const [ product, setProduct ] = useState([])
   const { id } = useParams()
-  const product = products.find((p) => String(p._id) === id)
-  if(!product) {
-    return (
-      <div>
-        Product not found!
-      </div>
-    )
-  }
+
+  useEffect(() => {
+    async function fetchProduct(){
+      const { data } = await axios.get(`/api/products/${id}`)
+      setProduct(data)
+    }
+
+    fetchProduct()  
+    
+  }, [id])
+  
+
   return (
     <div>
       <Link to='/' className='btn btn-light' my-3>Go Back</Link>
@@ -80,3 +85,13 @@ function ProductScreen() {
 }
 
 export default ProductScreen
+
+  // const { id } = useParams()
+  // const product = products.find((p) => String(p._id) === id)
+  // if(!product) {
+  //   return (
+  //     <div>
+  //       Product not found!
+  //     </div>
+  //   )
+  // }
