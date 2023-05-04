6397944f1c31aae301100ee82b8d6c3cb5897c92
progIfe <ifeoluwasamson90@gmail.com>
2023-05-04 23:51:38 +0100

    base urls.py syntax adjustments

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('products/', views.get_products, name='products'),
     path('products/<str:pk>/', views.get_product_detail, name='product-detail'),
     path('user/profile/', views.getUserProfile, name='user-profile'),
-    path('users/login/', views.MyTokenObtainPairView.as_view(),
+    path('user/login/', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
 ]
