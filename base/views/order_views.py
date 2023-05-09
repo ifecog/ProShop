f1393c14e8787bd382d82dbcb44dfc97e3b3f857
progIfe <ifeoluwasamson90@gmail.com>
2023-05-09 23:31:16 +0100

    oerder objects creation in the backend datbase by getting input data from the frontend PlaceOrderScreen

--- base/views/order_views.py
+++ base/views/order_views.py
@@ -10,3 +10,30 @@ from base.serializers import ProductSerializer
 from rest_framework import status
 
 
+@api_view(['POST'])
+@permission_classes(['IsAuthenticated'])
+def add_order_items(request):
+    user = request.user
+    data = request.data
+
+    order_items = data['OrderItem']
+
+    if order_items and len(order_items) == 0:
+        message = {'detail': 'No Order Items'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+    else:
+        # 1. create order
+        order = Order.objects.create(
+            user=user,
+            payment_method=data['paymentMethod'],
+            tax_price=data['taxFee'],
+            shpping_price=data['shippingFee'],
+            total_proce=data['totalFee'],
+
+        )
+        # 2. create shipping address
+        # 3. create order items and set order to orderItem relationship
+        # 4. update stock
+
+    return Response('ORDER')
