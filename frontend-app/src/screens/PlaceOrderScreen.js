6f41fab051f7b619105f77d215c6394c97c74e72
progIfe <ifeoluwasamson90@gmail.com>
2023-05-09 15:59:12 +0100

    Order Summary Table calculaions and conclusion

--- frontend-app/src/screens/PlaceOrderScreen.js
+++ frontend-app/src/screens/PlaceOrderScreen.js
@@ -7,7 +7,22 @@ import FormContainer from '../components/FormContainer'
 import CheckoutSteps from '../components/CheckoutSteps'
 
 function PlaceOrderScreen() {
-   const cart = useSelector(state => state.cart)
+    const cart = useSelector(state => state.cart)
+
+    cart.itemsPrice = cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2)
+
+//   I assume free shipping for a total price > $500, else $10 fee
+    cart.shippingFee = (cart.itemsPrice > 500 ? 0 : 10).toFixed(2)
+
+//    assuming a 8% sales tax
+    cart.taxFee = Number((0.08)  * cart.itemsPrice).toFixed(2)
+
+    cart.totalFee = (Number(cart.itemsPrice) + Number(cart.shippingFee) + Number(cart.taxFee)).toFixed(2)
+
+
+   const placeOrder = () => {
+    console.log('Place orders')
+   }
 
 
   return (
@@ -67,25 +82,25 @@ function PlaceOrderScreen() {
                         <ListGroup.Item>
                             <Row>
                                 <Col>Items: </Col>
-                                <Col md={4}>kjkjsd</Col>
+                                <Col md={4}>${cart.itemsPrice}</Col>
                             </Row>
                         </ListGroup.Item>
                         <ListGroup.Item>
                             <Row>
                                 <Col>Shipping: </Col>
-                                <Col md={4}>kjkjsd</Col>
+                                <Col md={4}>${cart.shippingFee}</Col>
                             </Row>
                         </ListGroup.Item>
                         <ListGroup.Item>
                             <Row>
                                 <Col>Tax: </Col>
-                                <Col md={4}>kjkjsd</Col>
+                                <Col md={4}>${cart.taxFee}</Col>
                             </Row>
                         </ListGroup.Item>
                         <ListGroup.Item>
                             <Row>
                                 <Col>Total: </Col>
-                                <Col md={4}>kjkjsd</Col>
+                                <Col md={4}>${cart.totalFee}</Col>
                             </Row>
                         </ListGroup.Item>
                         <ListGroup.Item>
@@ -93,6 +108,8 @@ function PlaceOrderScreen() {
                                 className='btn-block'
                                 style={{width: "100%"}} type='button'
                                 variant='primary'
+                                disabled={cart.cartItems.length === 0}
+                                onClick={placeOrder}
                             >
                                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
                                     Order
