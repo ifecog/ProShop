013b0603f092757afed50190e5caff951b6b4323
progIfe <ifeoluwasamson90@gmail.com>
2023-05-14 05:04:41 +0100

    get all orders by a user View

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -3,6 +3,7 @@ from base.views import order_views as views
 
 urlpatterns = [
     path('add/', views.addOrderItems, name='add-order'),
+    path('myorders/', views.getMyOrders, name='my-orders'),
     path('<str:pk>/', views.getOrderbyId, name='user-order'),
     path('<str:pk>/pay/', views.updateOrderToPaid, name='update-payment'),
 ]
