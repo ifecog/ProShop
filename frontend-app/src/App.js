71a222b90f15e2e5b7529dc0503a7a1c5df3a726
progIfe <ifeoluwasamson90@gmail.com>
2023-05-14 21:12:28 +0100

    Users Edit Screen and ProfileScreen editing

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,38 +1,40 @@
-import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import CartScreen from './screens/CartScreen'
-import LoginScreen from './screens/LoginScreen'
-import RegisterScreen from './screens/RegisterScreen'
-import ProfileScreen from './screens/ProfileScreen'
-import ShippingScreen from './screens/ShippingScreen'
-import PaymentScreen from './screens/PaymentScreen'
-import PlaceOrderScreen from './screens/PlaceOrderScreen'
-import OrderScreen from './screens/OrderScreen'
-import UserListScreen from './screens/UserListScreen'
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
+import UserEditScreen from "./screens/UserEditScreen";
 
 function App() {
   return (
     <Router>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
           <Routes>
-            <Route path='/' element={<HomeScreen />} exact />
-            <Route path='/product/:id' element={<ProductScreen />} />
-            <Route path='/cart/:id?' element={<CartScreen />} />
-            <Route path='/register' element={<RegisterScreen />} />
-            <Route path='/login' element={<LoginScreen />} />
-            <Route path='/profile' element={<ProfileScreen />} />
-            <Route path='/admin/userlist' element={<UserListScreen />} />
-            <Route path='/shipping' element={<ShippingScreen />} />
-            <Route path='/payment' element={<PaymentScreen />} />
-            <Route path='/placeorder' element={<PlaceOrderScreen />} />
-            <Route path='/order/:id' element={<OrderScreen />} />
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/cart/:id?" element={<CartScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
+            <Route path="/shipping" element={<ShippingScreen />} />
+            <Route path="/payment" element={<PaymentScreen />} />
+            <Route path="/placeorder" element={<PlaceOrderScreen />} />
+            <Route path="/order/:id" element={<OrderScreen />} />
+            <Route path="/admin/userlist" element={<UserListScreen />} />
+            <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
           </Routes>
         </Container>
       </main>
--- frontend-app/src/screens/ProfileScreen.js
+++ frontend-app/src/screens/ProfileScreen.js
@@ -1,186 +1,185 @@
-import React, { useState, useEffect } from 'react'
-import { Link, useNavigate, useLocation } from 'react-router-dom'
-import { Form, Button, Row, Col, Table } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import Message from '../components/Message'
-import FormContainer from '../components/FormContainer'
-import Loader from '../components/Loader'
-import { getUserDetails, updateUserProfile } from '../actions/userActions'
-import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
-import { listMyOrders } from '../actions/orderActions'
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Form, Button, Row, Col, Table } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import FormContainer from "../components/FormContainer";
+import Loader from "../components/Loader";
+import { getUserDetails, updateUserProfile } from "../actions/userActions";
+import { USER_UPDATE_PROFILE_RESET } from "../constants/userConstants";
+import { listMyOrders } from "../actions/orderActions";
 
 function ProfileScreen() {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [message, setMessage] = useState('')
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
 
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const userDetails = useSelector(state => state.userDetails)
-    const { error, loading, user } = userDetails
+  const userDetails = useSelector((state) => state.userDetails);
+  const { error, loading, user } = userDetails;
 
-    const userLogin = useSelector(state => state.userLogin)
-    const { userInfo } = userLogin
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-    const userUpdateProfile = useSelector(state => state.userUpdateProfile)
-    const { success } = userUpdateProfile
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const { success } = userUpdateProfile;
 
-    const orderListMy = useSelector(state => state.orderListMy)
-    const { loading: loadingOrders, error: errorOrders, orders } = orderListMy
+  const orderListMy = useSelector((state) => state.orderListMy);
+  const { loading: loadingOrders, error: errorOrders, orders } = orderListMy;
 
-    useEffect(() => {
-        if(!userInfo){
-            navigate('/login')
-        }else {
-            if(!user || !user.name || success) {
-                dispatch({ type:USER_UPDATE_PROFILE_RESET })
-                dispatch(getUserDetails('profile'))
-                dispatch(listMyOrders())
-            }
-            else {
-                setFirstName(user.first_name)
-                setLastName(user.last_name)
-                setEmail(user.email)
-            }
-        }
-    }, [navigate, userInfo, user, dispatch, success])
-
-    const submitHandler = (e) => {
-        e.preventDefault()
-
-        if(password !== confirmPassword) {
-            setMessage('Passwords do not match')
-        }else{
-            dispatch(updateUserProfile({
-                'id':user._id,
-                'first_name': firstName,
-                'last_name': lastName,
-                'email': email,
-                'password': password,
-            }))
-        }
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/login");
+    } else {
+      if (!user || !user.name || success || userInfo._id !== user._id) {
+        dispatch({ type: USER_UPDATE_PROFILE_RESET });
+        dispatch(getUserDetails("profile"));
+        dispatch(listMyOrders());
+      } else {
+        setFirstName(user.first_name);
+        setLastName(user.last_name);
+        setEmail(user.email);
+      }
     }
+  }, [navigate, userInfo, user, dispatch, success]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setMessage("Passwords do not match");
+    } else {
+      dispatch(
+        updateUserProfile({
+          id: user._id,
+          first_name: firstName,
+          last_name: lastName,
+          email: email,
+          password: password,
+        })
+      );
+    }
+  };
 
   return (
     <Row>
       <Col md={3}>
         <h2>User Profile</h2>
 
-        {message && <Message variant='danger'>{message}</Message>}
-        {error && <Message variant='danger'>{error}</Message>}
+        {message && <Message variant="danger">{message}</Message>}
+        {error && <Message variant="danger">{error}</Message>}
         {loading && <Loader />}
         <Form onSubmit={submitHandler}>
+          <Form.Group controlId="firstName" className="py-3">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              required
+              type="name"
+              placeholder="first name (reset?)"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-            <Form.Group controlId='firstName' className='py-3'>
-                <Form.Label>First Name</Form.Label>
-                <Form.Control
-                required
-                type='name'
-                placeholder='first name (reset?)'
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                >
-                </Form.Control>
-            </Form.Group>
+          <Form.Group controlId="lastName" className="py-3">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              required
+              type="name"
+              placeholder="last name (reset?)"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-            <Form.Group controlId='lastName' className='py-3'>
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control
-                required
-                type='name'
-                placeholder='last name (reset?)'
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                >
-                </Form.Control>
-            </Form.Group>
+          <Form.Group controlId="email" className="py-3">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              required
+              type="email"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-            <Form.Group controlId='email' className='py-3'>
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                required
-                type='email'
-                placeholder='email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                >
-            </Form.Control>
-            </Form.Group>
+          <Form.Group controlId="password" className="py-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="password (reset?)"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-            <Form.Group controlId='password' className='py-3'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control                
-                type='password'
-                placeholder='password (reset?)'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                >
-                </Form.Control>
-            </Form.Group>
+          <Form.Group controlId="passwordConfirm" className="py-3">
+            <Form.Label> Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-            <Form.Group controlId='passwordConfirm' className='py-3'>
-                <Form.Label> Confirm Password</Form.Label>
-                <Form.Control                
-                type='password'
-                placeholder='confirm password'
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                >
-                </Form.Control>
-            </Form.Group>
-        
-            <Button type='submit' variant='primary'>
+          <Button type="submit" variant="primary">
             Update
-            </Button>
+          </Button>
         </Form>
       </Col>
 
       <Col md={9}>
         <h2>My Orders</h2>
         {loadingOrders ? (
-            <Loader />
+          <Loader />
         ) : errorOrders ? (
-            <Message variant='danger'>{errorOrders}</Message>
+          <Message variant="danger">{errorOrders}</Message>
         ) : (
-            <Table striped responsive className='table-sm'>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Date</th>
-                        <th>Total($)</th>
-                        <th>Paid</th>
-                        <th>Delivered</th>
-                        <th></th>
-                    </tr>
-                </thead>
+          <Table striped responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Total($)</th>
+                <th>Paid</th>
+                <th>Delivered</th>
+                <th></th>
+              </tr>
+            </thead>
 
-                <tbody>
-                    {orders.map(order => (
-                        <tr key={order._id}>
-                            <td>{order._id}</td>
-                            <td>{order.createdTime.substring(0, 10)}</td>
-                            <td>{order.totalPrice}</td>
-                            <td>{order.isPaid ? order.paymentTime.substring(0, 10) : (
-                                <i className='fas fa-times' style={{ color:'red' }}></i>
-                            )}</td>
-                            <td>
-                                <LinkContainer to={`/order/${order._id}`}>
-                                    <Button className='btn-bt'>Details</Button>
-                                </LinkContainer>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdTime.substring(0, 10)}</td>
+                  <td>{order.totalPrice}</td>
+                  <td>
+                    {order.isPaid ? (
+                      order.paymentTime.substring(0, 10)
+                    ) : (
+                      <i className="fas fa-times" style={{ color: "red" }}></i>
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/order/${order._id}`}>
+                      <Button className="btn-bt">Details</Button>
+                    </LinkContainer>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
         )}
       </Col>
     </Row>
-  )
+  );
 }
 
-export default ProfileScreen
+export default ProfileScreen;
