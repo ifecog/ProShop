e3d0097f2b11506a077a30346925b26476d92bcf
progIfe <ifeoluwasamson90@gmail.com>
2023-05-25 18:06:59 +0100

    ORders List BAckend Endpoint

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from base.views import order_views as views
 
 urlpatterns = [
+    path('', views.getOrders, name='orders'),
     path('add/', views.addOrderItems, name='add-order'),
     path('myorders/', views.getMyOrders, name='my-orders'),
     path('<str:pk>/', views.getOrderbyId, name='user-order'),
--- base/views/order_views.py
+++ base/views/order_views.py
@@ -77,6 +77,15 @@ def getMyOrders(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getOrders(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getOrderbyId(request, pk):
     user = request.user
