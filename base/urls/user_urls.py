b6ca8711206d5b3116245e077e8b0e9aea840416
progIfe <ifeoluwasamson90@gmail.com>
2023-05-28 23:24:09 +0100

    Review POST request bug fix

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -3,11 +3,14 @@ from base.views import user_views as views
 
 urlpatterns = [
     path('', views.get_users, name='users '),
+    
     path('register/', views.register_user, name='register'),
     path('login/', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
+    
     path('profile/', views.get_user_profile, name='user-profile'),
     path('profile/update/', views.update_user_profile, name='update-user-profile'),
+    
     path('<str:pk>/', views.getUserById, name='get_user'),
     path('update/<str:pk>/', views.updateUser, name='update-user'),
     path('delete/<str:pk>/', views.deleteUser, name='delete-user'),
