8cce21425e0c1e0d631fc9afeb415356b8835f3b
progIfe <ifeoluwasamson90@gmail.com>
2023-05-29 15:34:54 +0100

    Product Search

--- frontend-app/src/components/SearchBox.js
+++ frontend-app/src/components/SearchBox.js
@@ -1,21 +1,29 @@
 import React, { useState } from "react"
 import { Button, Form } from "react-bootstrap"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useLocation } from "react-router-dom"
 
 function SearchBox() {
   const [keyword, setKeyword] = useState("")
 
+  const navigate = useNavigate()
+  const location = useLocation()
+
   const submitHandler = (e) => {
     e.preventDefault()
+    if (keyword) {
+      navigate(`/?keyword=${keyword}`)
+    } else {
+      navigate(navigate(location.pathname))
+    }
   }
 
   return (
-    <Form onSubmit={submitHandler} inline>
+    <Form onSubmit={submitHandler} inline className='d-flex'>
       <Form.Control
         type='text'
         name='q'
         onChange={(e) => setKeyword(e.target.value)}
-        className='mr-sm-2 ml-sm-5'
+        className='mr-2 mx-1'
       ></Form.Control>
 
       <Button type='submit' variant='outline-success' className='p-2'>
