03bbc99b50f3f778e7f36deadf1959768dc93f1c
progIfe <ifeoluwasamson90@gmail.com>
2023-05-29 20:37:59 +0100

    Top Rated Products constants and reducer

--- frontend-app/src/store.js
+++ frontend-app/src/store.js
@@ -12,6 +12,7 @@ import {
   productCreateReducer,
   productUpdateReducer,
   productCreateReviewReducer,
+  productTopRatedReducer,
 } from "./reducers/productReducers"
 import { cartReducer } from "./reducers/cartReducers"
 import {
@@ -39,6 +40,7 @@ const reducer = combineReducers({
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
   productCreateReview: productCreateReviewReducer,
+  productTopRated: productTopRatedReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
