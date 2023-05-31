afea7c15842018cf5336f1a7e1bc15037c3ddd65
progIfe <ifeoluwasamson90@gmail.com>
2023-05-31 01:31:18 +0100

    Adding HashRouter to frontend

--- backend/frontend-app/src/components/Header.js
+++ backend/frontend-app/src/components/Header.js
@@ -25,7 +25,7 @@ function Header() {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <SearchBox />
-            <Nav className='me-auto'>
+            <Nav className='ml-auto'>
               <LinkContainer to='/cart'>
                 <Nav.Link>
                   <i className='fas fa-shopping-cart' aria-hidden='true'></i>
